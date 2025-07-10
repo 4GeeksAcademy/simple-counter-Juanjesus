3287f72a2167fdf78c9033db4179a551c583bf0a
Juanje1997 <juanjesusdp1@outlook.es>
2025-07-10 10:20:41 +0000

    Finalizado

--- src/js/components/Home.jsx
+++ src/js/components/Home.jsx
@@ -1,28 +1,91 @@
 import React from "react";
 
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
+class Home extends React.Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			internalCounter: props.counter,
+			isRunning: true,
+			hasControl: false // si el usuario tocó algún botón
+		};
+		this.interval = null;
+	}
 
-//create your first component
-const Home = () => {
-	return (
-		<div className="text-center">
-            
+	componentDidMount() {
+		this.interval = setInterval(() => {
+			this.setState(prev => {
+				if (prev.isRunning) {
+					return { internalCounter: prev.internalCounter + 1 };
+				}
+				return null;
+			});
+		}, 1000);
+	}
 
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
-		</div>
-	);
-};
+	componentDidUpdate(prevProps) {
+		// Solo sincronizar con el prop mientras el usuario no haya tocado ningún botón
+		if (!this.state.hasControl && prevProps.counter !== this.props.counter) {
+			this.setState({ internalCounter: this.props.counter });
+		}
+	}
+
+	componentWillUnmount() {
+		clearInterval(this.interval);
+	}
+
+	handlePause = () => {
+		this.setState({ isRunning: false, hasControl: true });
+	};
+
+	handleResume = () => {
+		this.setState({ isRunning: true, hasControl: true });
+	};
+
+	handleReset = () => {
+		this.setState({ internalCounter: 0, isRunning: false, hasControl: true });
+	};
+
+	render() {
+		const { internalCounter, isRunning } = this.state;
+		const totalDigits = 6;
+		const digits = [];
+		let number = internalCounter;
+
+		for (let i = 0; i < totalDigits; i++) {
+			digits.unshift(number % 10);
+			number = Math.floor(number / 10);
+		}
+
+		return (
+			<div className="text-center">
+				<div className="container d-flex justify-content-center gap-3 mt-5 py-3">
+					<div className="reloj">
+						<i className="fa-regular fa-clock" style={{ color: "#ffffff" }}></i>
+					</div>
+					{digits.map((digit, index) => (
+						<div className="casilla" key={index}>
+							{digit}
+						</div>
+					))}
+				</div>
+
+				<div className="mt-3">
+					{isRunning ? (
+						<button className="btn btn-dark mx-2" onClick={this.handlePause}>
+							<span className="gap-2"><i className="fas fa-pause"></i> Pausar</span>
+						</button>
+					) : (
+						<button className="btn btn-dark mx-2" onClick={this.handleResume}>
+							<span className="gap-2"><i className="fas fa-play"></i> Reanudar</span>
+						</button>
+					)}
+					<button className="btn btn-dark mx-2" onClick={this.handleReset}>
+						<span className="gap-2"><i className="fa-solid fa-rotate-right"></i> Reiniciar</span>
+					</button>
+				</div>
+			</div>
+		);
+	}
+}
 
 export default Home;
--- src/js/main.jsx
+++ src/js/main.jsx
@@ -5,14 +5,23 @@ import ReactDOM from 'react-dom/client'
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap"
 
+
 // index.css'
 import '../styles/index.css'
 
 // components
 import Home from './components/Home';
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <Home/>
-  </React.StrictMode>,
-)
+const reactRender = ReactDOM.createRoot(document.getElementById('root'))
+
+let counter = 0
+
+setInterval(() => {
+  reactRender.render(
+    <React.StrictMode>
+      <Home counter={counter} />
+    </React.StrictMode>,
+  )
+  counter++
+}, 1000)
+
